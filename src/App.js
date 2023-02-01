23e1f3d10b1ebe2c9b74fd299d87f94e827bdbcb
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-01 16:20:50 -0600

    Completed lesson 1
    added all the demo code in the videos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from "react";
+// import logo from './logo.svg';
+import { Greeting } from "./Greeting";
+import { PeopleList } from "./PeopleList";
 import './App.css';
 
+const people = [{
+  name: 'Matthew',
+  age: 40,
+  hairColor: 'brown',
+}, {
+  name: 'Mallory',
+  age: 17,
+  hairColor: 'red',
+}, {
+  name: 'Brian',
+  age: 55,
+  hairColor: 'Blonde'
+}];
+
+
 function App() {
-  return (
+  let adjective = 'Ipsum';
+  let url = "https://reactjs.org"
+
+  return (    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
 }
-
 export default App;
+
+
+//   <Greeting name="Sam" numberOfMessages={5} /> {/*person={{ name: "Sam", numberOfMessages: 5}} object in react*/}
+{/* <PeopleList people={people}/>
+<button onClick={()=> alert('hello')}>Click Me!</button>
+<p>
+  lorem {adjective}!
+</p>
+<a
+  className="App-link"
+  href={url}
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a> */}
