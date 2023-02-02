1633387c05256966b771102958a0e051488352ec
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-01 23:26:59 -0600

    Got Bootstrap Working great!
    import 'bootstrap/dist/css/bootstrap.min.css';

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -4,19 +4,42 @@ import { NavLink, Outlet } from "react-router-dom"
 export default function RootLayout() {
   return (
     <div className='root-layout'>
-        <header>
-            <nav>
-                <h1>Jobarouter</h1>
-                <NavLink to="HomePage" className={"nav-link"}>Home</NavLink>
-                <NavLink to="CounterButtonPage">Counter Button</NavLink>
-                <NavLink to="PeopleListPage">People List</NavLink>
-            </nav>
-        </header>
+      <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+            <h1>Homepage</h1>
+            <li class="nav-item">
+              <NavLink to="HomePage" className={"nav-link"}>Home</NavLink>
+            </li>
+              <NavLink to="CounterButtonPage" className={"nav-link"}>Counter Button</NavLink>
+              <NavLink to="PeopleListPage" className={"nav-link"}>People List</NavLink>
+          </div>
+        </nav>
+      </header>
 
         <main>
             <Outlet/>
         </main>
-
     </div>
   )
 }
+<nav >
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+          <a class="nav-link" href="#">Link</a>
+        
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+
+</nav>
